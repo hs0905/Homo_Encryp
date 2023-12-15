eaa0d258071428a906060844e5e13e985635d1ea
hs0905 <blacksmith13592@gmail.com>
2023-12-15 17:11:19 +0900

    add table_sort, start_point function

--- fhe_benes/fhe_interconnect/step1/C_code/CPP/benes_routing.cpp
+++ fhe_benes/fhe_interconnect/step1/C_code/CPP/benes_routing.cpp
@@ -4,30 +4,26 @@
 
 const int SIZE = 32;
 
-void get_num(std::array<int, SIZE>& arr) {
+void get_num(std::array<int, SIZE>& arr) 
+{
     bool isValid;
     do {
         isValid = true;
-
         for (int i = 0; i < SIZE; i++) {
             std::cin >> arr[i];
-
             // 범위 검사
             if (arr[i] < 0 || arr[i] >= SIZE) {
-                std::cout << "Error: Number out of range. Please enter all numbers again." << std::endl;
+                std::cout << "Error" << std::endl;
                 isValid = false;
                 break; // 중단하고 다시 시작
             }
-
-            // 중복 검사
             for (int j = 0; j < i; j++) {
                 if (arr[j] == arr[i]) {
-                    std::cout << "Error: Duplicate number found. Please enter all numbers again." << std::endl;
+                    std::cout << "Error" << std::endl;
                     isValid = false;
                     break; // 중단하고 다시 시작
                 }
             }
-
             if (!isValid) {
                 break; // 중단하고 다시 시작
             }
@@ -35,15 +31,47 @@ void get_num(std::array<int, SIZE>& arr) {
     } while (!isValid); // 유효하지 않은 입력이 있으면 반복
 }
 
+void table_sort(std::array< std::array<int, 2>, SIZE*2> & table){
+  int reverse_table[SIZE][2];
+  for(int i = 0; i< SIZE; i++){
+    reverse_table[i][1] = table[i][0];
+    reverse_table[i][0] = table[i][1];
+  }
+  for(int i = 0; i < SIZE-1 ; i++){
+    int min_index = i;
+    for(int j = i+1; j< SIZE ; j++){
+      if(reverse_table[j][0] > reverse_table[min_index][0]){
+        min_index = j;
+        }
+        if(min_index != i){
+          std::swap(reverse_table[i][0], reverse_table[min_index][0]); 
+          std::swap(reverse_table[i][1], reverse_table[min_index][1]);
+        }
+    }
+  }
+  for(int i = SIZE ; i < SIZE*2 ; i++){
+    table[i][0] = reverse_table[i-SIZE][0];
+    table[i][1] = reverse_table[i-SIZE][1];
+  }
+}
+
+int start_point(std::array< std::array<int, 2>, SIZE*2> & table){
+  return table[SIZE][0];
+}
+
 int main() {
     std::array<int, SIZE> benes_num;
     get_num(benes_num);
 
-    // 배열 출력
-    for (int i = 0; i < SIZE; i++) {
-        std::cout << benes_num[i] << " ";
-    }
-    std::cout << std::endl;
-
+   std::array< std::array<int, 2>, SIZE*2> table;
+    for(int i=0; i<SIZE; i++){
+        table[i][0] = i;
+        table[i][1] = benes_num[i];
     return 0;
+    table_sort(table);
+    int start = start_point(table);
+
 }
+}
+
+
