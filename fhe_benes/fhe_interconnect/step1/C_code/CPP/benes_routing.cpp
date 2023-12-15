26dc7f9ab508e013edbaa29d99462aa85f53e18f
hs0905 <blacksmith13592@gmail.com>
2023-12-15 16:10:06 +0900

    add the validation check

--- fhe_benes/fhe_interconnect/step1/C_code/CPP/benes_routing.cpp
+++ fhe_benes/fhe_interconnect/step1/C_code/CPP/benes_routing.cpp
@@ -1,27 +1,49 @@
-#include<iostream>
-#include<cmath>
-#include<array>
-#include<vector>
+#include <iostream>
+#include <array>
+#include <algorithm> // std::find 함수를 사용하기 위해 필요
 
 const int SIZE = 32;
-const int SW_SIZE = SIZE/2;
 
-void get_num(std::array<int, SIZE>& arr){
-  for(int i = 0; i < SIZE; i++){
-    std::cin >> arr[i]; // get the input for the benes network
-  }
+void get_num(std::array<int, SIZE>& arr) {
+    bool isValid;
+    do {
+        isValid = true;
+
+        for (int i = 0; i < SIZE; i++) {
+            std::cin >> arr[i];
+
+            // 범위 검사
+            if (arr[i] < 0 || arr[i] >= SIZE) {
+                std::cout << "Error: Number out of range. Please enter all numbers again." << std::endl;
+                isValid = false;
+                break; // 중단하고 다시 시작
+            }
+
+            // 중복 검사
+            for (int j = 0; j < i; j++) {
+                if (arr[j] == arr[i]) {
+                    std::cout << "Error: Duplicate number found. Please enter all numbers again." << std::endl;
+                    isValid = false;
+                    break; // 중단하고 다시 시작
+                }
+            }
+
+            if (!isValid) {
+                break; // 중단하고 다시 시작
+            }
+        }
+    } while (!isValid); // 유효하지 않은 입력이 있으면 반복
 }
 
-int main()
-{
-  std::array<int, SIZE> benes_num;
-  get_num(benes_num);
-  for(int i = 0; i < SIZE; i++){
-    std::cout << benes_num[i];
+int main() {
+    std::array<int, SIZE> benes_num;
+    get_num(benes_num);
+
+    // 배열 출력
+    for (int i = 0; i < SIZE; i++) {
+        std::cout << benes_num[i] << " ";
     }
-  
-  return 0;
+    std::cout << std::endl;
+
+    return 0;
 }
-
-
-
