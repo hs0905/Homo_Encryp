3e377aa709b89fad3c4ee6b36db44f82d32267cb
hs0905 <blacksmith13592@gmail.com>
2023-12-28 02:30:59 +0900

    before big change

--- fhe_benes/fhe_interconnect/step1/RTL/sim_resource/benes/network_module.sv
+++ fhe_benes/fhe_interconnect/step1/RTL/sim_resource/benes/network_module.sv
@@ -5,9 +5,9 @@ import FHE_ALU_PKG::*;
 
 module	network_module(
 	input logic clk, //clock
-	input logic [DATA_WIDTH-1:0] i_port [0:SIZE-1], //input port bit size : 512
-  input logic [SWITCH_NUM-1:0]switch_set[0:STAGE_NUM-1],//switch set for each stage
-	output logic [DATA_WIDTH-1:0] o_port [0:SIZE-1] //output port bit size : 512
+	input logic [DATA_WIDTH-1:0] 	i_port 		[0:SIZE-1], //input port bit size : 512
+  input logic [SWITCH_NUM-1:0]	switch_set[0:STAGE_NUM-1],//switch set for each stage
+	output logic [DATA_WIDTH-1:0] o_port 		[0:SIZE-1] //output port bit size : 512
 );
 
 
@@ -55,6 +55,11 @@ stage_module stage_8(
 );
 
 endmodule
+
+
+
+
+
 
 module	type_network_module(
 	input 	logic clk, //clock
--- fhe_benes/fhe_interconnect/step1/RTL/sim_resource/benes/switch_module.sv
+++ fhe_benes/fhe_interconnect/step1/RTL/sim_resource/benes/switch_module.sv
@@ -3,12 +3,12 @@ import FHE_ALU_PKG::*;
 `timescale 1ns/1ns
 
 module switch_module(
-    input logic [DATA_WIDTH-1:0]i_port_0,
-    input logic [DATA_WIDTH-1:0]i_port_1,
-    input logic clk,
-    input logic switch_set,
-    output logic [DATA_WIDTH-1:0]o_port_0,
-    output logic [DATA_WIDTH-1:0]o_port_1
+    input logic [DATA_WIDTH-1:0]    i_port_0,
+    input logic [DATA_WIDTH-1:0]    i_port_1,
+    input logic                     clk,
+    input logic                     switch_set,
+    output logic [DATA_WIDTH-1:0]   o_port_0,
+    output logic [DATA_WIDTH-1:0]   o_port_1
 );
 
 always_comb begin
@@ -16,8 +16,7 @@ always_comb begin
     begin
         o_port_0 = i_port_0;
         o_port_1 = i_port_1;        
-    end
-    else begin
+    end else begin
         o_port_0 = i_port_1;
         o_port_1 = i_port_0;
     end
